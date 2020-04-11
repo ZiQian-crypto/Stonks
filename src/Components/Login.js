d6428d5714a6266d6379dfa1dac9340f279253ea
arjoooooon <arjun.taneja02@gmail.com>
2020-04-12 03:22:44 +0800

    Login modifications

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -13,10 +13,10 @@ function Login(){
                 <input className="login-detail" placeholder="Your email"/>
                 <div className="login-label">Password</div>
                 <input type="password" className="login-detail" placeholder="Your password"/>
-                <div style={{margin: "5% 0 0.5% 0"}}>
+                <div style={{margin: "5% 0 0 0"}}>
                     <button className="login-submit" onClick={handleLogin}>Login</button>
                 </div>
-                <div style={{margin: "5% 0 0.5% 0"}}>
+                <div style={{margin: "5% 0 0 0"}}>
                     <button className="google-login-button" onClick={handleLogin}>Sign in with Google</button>
                 </div>
 
