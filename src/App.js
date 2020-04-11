aa2c52f6cbd191d21a493a23e5f11b05a02641f4
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-04-12 05:54:37 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Login from './Components/Login';
+import Home from './Components/Home';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App(){
     return (
-        <Login />
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
