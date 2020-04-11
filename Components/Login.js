dd6ca66f9ddd6d967aa396ba1172d11e376abf18
arjoooooon <arjun.taneja02@gmail.com>
2020-04-12 03:16:13 +0800

    Add sample login page

--- Components/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Login(){
-    return (
-        <div>
-            <span>This is a login page!</span>
-        </div>
-    );
-}
---
+++ src/Components/Login.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import '../Styles/Login.css';
+
+function Login(){
+    
+    const handleLogin = () => {
+        alert("Feature not implemented yet!")
+    }
+    return (
+        <div className="login-wrapper">
+            <div className="login-window">
+                <div className="login-label">Email</div>
+                <input className="login-detail" placeholder="Your email"/>
+                <div className="login-label">Password</div>
+                <input type="password" className="login-detail" placeholder="Your password"/>
+                <div style={{margin: "5% 0 0.5% 0"}}>
+                    <button className="login-submit" onClick={handleLogin}>Login</button>
+                </div>
+                <div style={{margin: "5% 0 0.5% 0"}}>
+                    <button className="google-login-button" onClick={handleLogin}>Sign in with Google</button>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Login;
