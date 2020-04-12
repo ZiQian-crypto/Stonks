9cbad876b812833c4b2d9aa5b4f1475ca680ec7b
arjoooooon <arjun.taneja02@gmail.com>
2020-04-12 17:23:07 +0800

    hamburger removal for good

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -35,12 +35,6 @@ function Dashboard(){
                 className = "card-input"
                 />
                 <Carousel resultCount = {10} />
-<<<<<<< HEAD
-            </div>
-            <div className="hamburger-icon">
-                <Hamburger />
-=======
->>>>>>> 076ea6016b2a136928fbe42c6c2915f175223def
             </div>
         </div>
     )
