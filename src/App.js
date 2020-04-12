29d73291fe0c32d04e0200d5a9938c980411659c
arjoooooon <arjun.taneja02@gmail.com>
2020-04-12 11:29:20 +0800

    Started routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
 import UserProvider from './Providers/UserProvider';
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 function App(){
     return (
         <UserProvider>  
-            <Login />
-            <Dashboard />            
+            <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+                <Route exact path="/dashboard">
+                    <Dashboard />
+                </Route>
+            </Switch>
+           </Router>
         </UserProvider>
     );
 }
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../Providers/UserProvider';
+import '../Styles/Dashboard.css';
 
 function Dashboard(){
 
     let user = useContext(UserContext);
 
     return(
-        <div>
-            <span>
-                Hello {(user === null)? '' : user.displayName}!
-            </span>
+        <div className='dash-background'>
+           <span>Stuff</span> 
         </div>
     )
 }
