64604977b1c54eb26c5dbe570ef02496d04210d8
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-04-12 23:04:29 +0800

    finally done

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,7 +3,6 @@ import Geocode from 'react-geocode';
 import '../Styles/Carousel.css';
 
 function reverseGeocode(lat, lng) {
-
     Geocode.setApiKey("AIzaSyDVtxfNIxil5oppS_s3SlMP1Kn0mBnKuAE");
     Geocode.fromLatLng(lat, lng).then( response => {
         let address = response.results[0].formatted_address;
@@ -12,8 +11,15 @@ function reverseGeocode(lat, lng) {
     }, error => {
         console.log(error)
     });
+}
 
-    return "Hello";
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 export default class Carousel extends Component {
@@ -23,22 +29,26 @@ export default class Carousel extends Component {
 
         return (
             <div className="result-container">
+                <a href="https://www.mustafa.com.sg/" target="_blank">
                     {data.map((supplier) => {
                         return <div className={"card -" + supplier.id}>
-                            <div className="card-inner-left"></div>
+                            <div className="card-inner-left">
+                                <div className="store-image" style={{ "background-color": getRandomColor() }}></div>
+                            </div>
                             <div className="card-inner-right">
                                 {/* store details go here */}
                                 <div className="store-title">{supplier.name}</div>
                                 <div className="store-address">
-                                    {(supplier.currentProduct === null)? '' : supplier.currentProduct}
+                                    {supplier.location}
                                 </div>
                                 <div className="store-response">
-                                    { supplier.location }
+                                    {(supplier.currentProduct === null) ? '' : supplier.currentProduct}
                                 </div>
                                 <div className="store-distance"></div>
                             </div>
                         </div>
                     })}
+                </a>
             </div>
         )
     }
