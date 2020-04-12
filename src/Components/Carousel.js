0befaa1306af2eedc80f2bdc6f609f0d7991120f
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-04-12 20:23:51 +0800

    firebase linked

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -9,18 +9,18 @@ export default class Carousel extends Component {
 
         return (
             <div className="result-container">
-                {data.map((store) => {
-                    return <div className={"card -" + store.id}>
-                        <div className="card-inner-left"></div>
-                        <div className="card-inner-right">
-                            {/* store details go here */}
-                            <div className="store-title">{store.name}</div>
-                            <div className="store-address"></div>
-                            <div className="store-response"></div>
-                            <div className="store-distance"></div>
+                    {data.map((supplier) => {
+                        return <div className={"card -" + supplier.id}>
+                            <div className="card-inner-left"></div>
+                            <div className="card-inner-right">
+                                {/* store details go here */}
+                                <div className="store-title">{supplier.name}</div>
+                                <div className="store-address"></div>
+                                <div className="store-response"></div>
+                                <div className="store-distance"></div>
+                            </div>
                         </div>
-                    </div>
-                })}
+                    })}
             </div>
         )
     }
