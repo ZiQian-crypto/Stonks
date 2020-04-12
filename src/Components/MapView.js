a9ea2ed1928ea042b3c265c6dcb39dae41a7eccd
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-04-12 12:39:48 +0800

    Idk bro

--- src/Components/MapView.js
+++ src/Components/MapView.js
@@ -16,7 +16,7 @@ export default class MapView extends Component {
     render() {
         const position = [this.state.lat, this.state.lng]
         return(
-            <Map center={position} zoom={this.state.zoom} >
+            <Map center={position} zoom={this.state.zoom} style={{ width: 100 px, height: 100 px, }}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
